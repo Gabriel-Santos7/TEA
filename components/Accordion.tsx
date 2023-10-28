1f07c9fdaab078c1d5e849664d3f69e448cf33b4
Borges <gabrielborgessantos721@gmail.com>
2023-10-28 17:41:56 -0300

    feat: landing page

---
+++ components/Accordion.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import React, { useState } from "react";
+
+interface AccordionProps {
+  title: string;
+  content: string;
+}
+
+const Accordion: React.FC<AccordionProps> = ({ title, content }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div
+      className={`border border-gray-300 rounded-md mb-4 ${
+        isOpen ? "bg-white" : ""
+      }`}
+    >
+      <div
+        className="flex items-center justify-between p-4 cursor-pointer"
+        onClick={toggleAccordion}
+      >
+        <h2 className="text-lg font-medium text-black">{title}</h2>
+        <svg
+          className={`w-6 h-6 transform transition-transform ${
+            isOpen ? "rotate-90" : "rotate-0"
+          }`}
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d={isOpen ? "M12 5l7 7-7 7" : "M9 5l7 7-7 7"}
+          />
+        </svg>
+      </div>
+      <div
+        className={`${
+          isOpen && "p-4"
+        } transition-max-h max-h-0 overflow-hidden ${
+          isOpen ? "max-h-full" : ""
+        }`}
+      >
+        <p
+          className={`text-gray-700 transition-opacity ${
+            isOpen ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          {content}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Accordion;
---
+++ components/Navbar.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+
+export const Navbar = () => {
+  return (
+    <nav className="bg-[#eaeaea] border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="https://flowbite.com/" className="flex items-center">
+          <Image
+            src="http://autismoerealidade.org.br/wp-content/uploads/2019/02/logo_autismo_color.svg"
+            className="h-8 mr-3"
+            alt="Autismo e realidade Logo"
+            width={200}
+            height={200}
+          />
+        </a>
+
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                aria-current="page"
+              >
+                Convivendo com o Tea
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Quem somos?
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
