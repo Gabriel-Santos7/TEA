0b8b8a20fd4036e9cbb2e9efefd219d539af404a
Borges <gabrielborgessantos721@gmail.com>
2023-11-07 22:58:59 -0300

    navbar refactor

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,39 +1,96 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
-export const Navbar = () => {
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState("");
+
   return (
-    <nav className="bg-[#eaeaea] border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center">
-          <Image
-            src="http://autismoerealidade.org.br/wp-content/uploads/2019/02/logo_autismo_color.svg"
-            className="h-8 mr-3"
-            alt="Autismo e realidade Logo"
-            width={200}
-            height={200}
-          />
-        </a>
-
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
+    <nav className="bg-white dark:bg-gray-800 z-50">
+      <div className="max-w-7xl mx-auto px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex justify-between w-full items-center">
+            <a className="flex-shrink-0" href="/">
+              LOGO
+            </a>
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <a
+                  href="#"
+                  onClick={() => setActiveItem("tea")}
+                  className={`${
+                    activeItem === "tea"
+                      ? "text-white"
+                      : "text-gray-300 hover:text-white"
+                  } dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium`}
+                >
+                  Convivendo com o Tea
+                </a>
+                <a
+                  href="#"
+                  onClick={() => setActiveItem("somos")}
+                  className={`${
+                    activeItem === "somos"
+                      ? "text-white"
+                      : "text-gray-300 hover:text-white"
+                  } dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium`}
+                >
+                  Quem somos?
+                </a>
+              </div>
+            </div>
+            <div className="md:hidden">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-gray-300 focus:outline-none focus:text-gray-500 focus:bg-gray-100 transition duration-150 ease-in-out"
+              >
+                <svg
+                  className="h-6 w-6"
+                  stroke="currentColor"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div
+            className={`${
+              isOpen ? "block animate-fade-in-down" : "hidden"
+            } md:hidden`}
+          >
+            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 mt-20">
               <a
                 href="#"
-                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                aria-current="page"
+                onClick={() => setActiveItem("tea")}
+                className={`${
+                  activeItem === "tea"
+                    ? "text-white"
+                    : "text-gray-300 hover:text-white"
+                }
+                mt-2 bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium`}
               >
                 Convivendo com o Tea
               </a>
-            </li>
-            <li>
               <a
                 href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                onClick={() => setActiveItem("somos")}
+                className={`${
+                  activeItem === "somos"
+                    ? "text-white"
+                    : "text-gray-300 hover:text-white"
+                } bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium`}
               >
                 Quem somos?
               </a>
-            </li>
-          </ul>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
