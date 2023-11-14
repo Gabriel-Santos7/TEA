1fe8acf26bc34497935df83fd42b03bf067b5a32
Gabriel Borges <gabrielborgessantos721@gmail.com>
2023-11-14 11:34:48 -0300

    Update teamMembers.ts

--- utils/teamMembers.ts
+++ utils/teamMembers.ts
@@ -1,6 +1,6 @@
 export const teamMembers = [
   { name: "Eduarda dos Santos da Costa", role: "Função 1" },
-  { name: "Giulia Cipriano Sampaio", role: "Função 2" },
+  { name: "Giulia Sampaio Cipriano", role: "Função 2" },
   { name: "Roseane Borges dos Santos", role: "Função 3" },
   { name: "Vitória Rosa dos Santos", role: "Função 4" },
 ];
